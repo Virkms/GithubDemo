29c57f0f38807374213f6d0b531b17ea138256be
@Virkms <>
2023-01-25 16:51:27 -0500

    GithubDemo

---
+++ src/main/java/virk/manpreet/githubdemo/GithubDemo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package virk.manpreet.githubdemo;
+
+/**
+ *
+ * @author sukhm
+ */
+public class GithubDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
